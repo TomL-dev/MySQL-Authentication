2789af855e37b646e08ba49cb159c80dbf38728d
TomL-dev <tomliefheid@gmail.com>
2017-09-28 15:45:50 +0200

    added database functions

--- functions/functions.js
+++ functions/functions.js
@@ -1,7 +1,30 @@
 var user = require('../model/user');
+var db = require('../model/database');
+
+module.exports.Authenticate = Authenticate;
+module.exports.CreateResponse = CreateResponse;
+module.exports.DatabaseSelect = DatabaseSelect;
+
 
 function Authenticate(username, password, callback){
 	console.log("Authenticate: " + username);
+	// search database for users with this name
+	db.getConnection( (err, conn) => {
+		if (err){
+			console.log(err);
+		} else {
+			var query = "";
+			this.DatabaseSelect(conn, query).then(function(data){
+				if (data.results.length > 0){
+					// user found.
+				} else {
+					// no user found
+				}
+			}).catch(function(error){
+
+			});
+		}
+	});
 	var newUser = new user(username);
 	newUser.password = password;
 	callback(null, newUser);
@@ -16,5 +39,17 @@ function CreateResponse(message, shortcode){
 	return obj;
 }
 
-module.exports.Authenticate = Authenticate;
-module.exports.CreateResponse = CreateResponse;
+function DatabaseSelect(connection, query){
+	return new Promise(function(resolve, reject){
+		connection.query(query, function(error, results, fields){
+			connection.release();
+			if (error){
+				reject(Error(error));
+			} else {
+				var obj = {"results" : results, "fields" : fields};
+				resolve(obj);
+			}
+		});
+	});
+}
+
